07d7874ca8ebb862825c4f4fd105ccb5c608ba93
jamesricarte <126384275+jamesricarte@users.noreply.github.com>
2024-05-17 14:37:35 +0800

    added user sql table sa sql database

--- refilling_station.sql
+++ refilling_station.sql
@@ -1,3 +1,11 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `create_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE `customers` (
   `customer_Id` int(11) AUTO_INCREMENT PRIMARY KEY,
   `customer_Name` varchar(55) DEFAULT NULL,
